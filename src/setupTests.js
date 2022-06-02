2d7d6c2c672edfc875b6f05a719db184088ed107
Tomas Sanchez <tms90@outlook.es>
2022-06-02 18:43:39 +0200

    Added route navigate to /register

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+
+import { server } from "./mocks/server.js";
+import store from "./redux/store/index.js";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const renderWithProviders = (component) => {
+  const Providers = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return render(component, { wrapper: Providers });
+};
+
+export default renderWithProviders;

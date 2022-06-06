c86e860ee8a17bad6efbf9cf5de3bf431871242d
Tomas Sanchez <tms90@outlook.es>
2022-06-06 16:03:30 +0200

    Added Coffeeshop component + css and the enrouted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import CoffeeShopPage from "./pages/CoffeeShopPage/CoffeeShopPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/coffeeshops" element={<CoffeeShopPage />} />
       </Routes>
     </>
   );
---
+++ src/components/CoffeeShop/CoffeeShop.js
@@ -0,0 +1,80 @@
+import CoffeShopStyled from "./CoffeeShopStyled";
+
+const CoffeeShop = () => {
+  return (
+    <>
+      <CoffeShopStyled>
+        <div className="container">
+          <div className="coffeeshop-image">
+            <img
+              src="https://morrowcoffee.com/wp-content/uploads/2020/04/IMG_5154.jpg"
+              alt="Morrow Coffee"
+              className="coffeshop__img"
+            />
+          </div>
+          <div className="coffeshop-info">
+            <div className="coffeshop-name">
+              <span className="name">Morrow Coffee</span>
+            </div>
+            <div className="info">
+              <span> Av. Gran via de les cortes catalanes </span>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="coffeeshop-image">
+            <img
+              src="https://morrowcoffee.com/wp-content/uploads/2020/04/IMG_5154.jpg"
+              alt="Morrow Coffee"
+              className="coffeshop__img"
+            />
+          </div>
+          <div className="coffeshop-info">
+            <div className="coffeshop-name">
+              <span className="name">Morrow Coffee</span>
+            </div>
+            <div className="info">
+              <span> Av. Gran via de les cortes catalanes </span>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="coffeeshop-image">
+            <img
+              src="https://morrowcoffee.com/wp-content/uploads/2020/04/IMG_5154.jpg"
+              alt="Morrow Coffee"
+              className="coffeshop__img"
+            />
+          </div>
+          <div className="coffeshop-info">
+            <div className="coffeshop-name">
+              <span className="name">Morrow Coffee</span>
+            </div>
+            <div className="info">
+              <span> Av. Gran via de les cortes catalanes </span>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="coffeeshop-image">
+            <img
+              src="https://morrowcoffee.com/wp-content/uploads/2020/04/IMG_5154.jpg"
+              alt="Morrow Coffee"
+              className="coffeshop__img"
+            />
+          </div>
+          <div className="coffeshop-info">
+            <div className="coffeshop-name">
+              <span className="name">Morrow Coffee</span>
+            </div>
+            <div className="info">
+              <span> Av. Gran via de les cortes catalanes </span>
+            </div>
+          </div>
+        </div>
+      </CoffeShopStyled>
+    </>
+  );
+};
+
+export default CoffeeShop;

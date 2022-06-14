38b227bfae4fdf6d0729b9d78203b653d4fdf3ef
Tomas Sanchez <tms90@outlook.es>
2022-06-14 09:49:41 +0200

    Tested register reducer of userSlice

--- src/redux/features/userSlice.test.js
+++ src/redux/features/userSlice.test.js
@@ -1,3 +1,4 @@
+import userSlice from "./userSlice";
 import userReducer, { registerActionCreator } from "./userSlice";
 
 describe("Given a userReducer", () => {
@@ -14,6 +15,23 @@ describe("Given a userReducer", () => {
       const newState = userReducer(initialValue, action);
 
       expect(newState).toEqual(receivedValue);
+    });
+  });
+});
+
+describe("Given a user register reducer", () => {
+  describe("When it receives an initial state status and a register action with the register user credentials", () => {
+    test("Then it should return the new user state with the received user credentials and the logged property false", () => {
+      const initialState = { username: "", name: "", id: "" };
+
+      const userInfo = { username: "pepito", name: "pepe", id: "dfdwedd2w" };
+
+      const expectedState = userInfo;
+
+      const registerAction = registerActionCreator(userInfo);
+      const userStatus = userSlice(initialState, registerAction);
+
+      expect(userStatus).toEqual(expectedState);
     });
   });
 });

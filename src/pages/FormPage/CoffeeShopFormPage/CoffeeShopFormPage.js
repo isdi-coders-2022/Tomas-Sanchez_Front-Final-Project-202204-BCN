ff28208d5087c4a734d5c83d62ca8aaee9dc579e
Tomas Sanchez <tms90@outlook.es>
2022-06-15 14:02:37 +0200

    Fixed css wraped error in EditCoffeeShopForm

--- src/pages/FormPage/CoffeeShopFormPage/CoffeeShopFormPage.js
+++ src/pages/FormPage/CoffeeShopFormPage/CoffeeShopFormPage.js
@@ -1,28 +1,11 @@
-import styled from "styled-components";
 import CoffeeShopForm from "../../../components/CoffeeShopForm/CoffeeShopForm";
-
-const CoffeeShopFormPageContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  background-image: url("image/fondo de pantalla.jpg");
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  height: 100vh;
-  background-size: cover;
-
-  a {
-    font-size: 20px;
-  }
-`;
+import { CoffeeShopStyle } from "../CoffeeShopStyle";
 
 const CoffeeShopFormPage = () => {
   return (
-    <CoffeeShopFormPageContainer>
+    <CoffeeShopStyle>
       <CoffeeShopForm />
-    </CoffeeShopFormPageContainer>
+    </CoffeeShopStyle>
   );
 };
 
